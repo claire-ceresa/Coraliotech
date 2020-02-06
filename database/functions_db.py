3845994f0024f7aa126ee0a1f75993487f32b29f
CCeresa <claire.ceresa@hotmail.fr>
2020-02-06 10:32:51 +0100

    Creation des fonctions de gestion de la base de données

--- database/functions_db.py
+++ database/functions_db.py
@@ -1,30 +1,37 @@
 import sqlite3 as s
 
-def insert_application_possible(datas):
-    connection = s.connect("database\coraliotech_test_db.db")
+database = "database\coraliotech_test_db.db"
+
+
+def commit_query(query):
+    connection = s.connect(database)
     cursor = connection.cursor()
-    query_format = "INSERT INTO Applications_possibles (nom) VALUES (\"{nom}\")"
-    query = query_format.format(nom=datas["nom"])
     cursor.execute(query)
     connection.commit()
     connection.close()
 
-def insert_organisme(datas):
-    connection = s.connect("database\coraliotech_test_db.db")
+
+def execute_query(query):
+    connection = s.connect(database)
     cursor = connection.cursor()
-    query_format = "INSERT INTO Organisme VALUES " \
-                   "(\"{espece}\", \"{genre}\", \"{famille}\", \"{ordre}\", \"{sous_classe}\", \"{classe}\", \"{embranchement}\", {statut})"
-    query = query_format.format(espece=datas["espece"], genre=datas["genre"], famille=datas["famille"], ordre=datas["ordre"], sous_classe=datas["sous_classe"], classe=datas["classe"], embranchement=datas["embranchement"], statut=datas["statut"])
     cursor.execute(query)
-    connection.commit()
+    result = cursor.fetchall()
     connection.close()
+    return result
+
+
+def get_query_insert(table, datas):
+    begin = "INSERT INTO " + table + " ( "
+    keys = (" , ").join(datas.keys())
+    middle = " ) VALUES ( "
+    values = (" , ").join(list(datas.values()))
+    end = " );"
+    query = begin + keys + middle + values + end
+    return query
+
 
 def get_applications_possibles():
-    connection = s.connect("database\coraliotech_test_db.db")
-    cursor = connection.cursor()
     query = "SELECT nom FROM Applications_possibles"
-    cursor.execute(query)
-    resultat = cursor.fetchall()
-    connection.close()
-    return resultat
+    result = execute_query(query)
+    return result
 
